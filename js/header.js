48a3e148fb78ad4de7b7fe67b19f63e3ac6b9a10
mmcxph <mmcxph@gmail.com>
2022-08-12 10:52:25 +0000

    Added header.js

--- js/header.js
+++ js/header.js
@@ -1,19 +1,37 @@
-function animateIcon(x) {
+function animateIcon() {
+    // change X to E in burger icon
+    let x = document.getElementById("burger-icon");
     x.classList.toggle("change");
-}
-
-function mobileMenu() {
-    let burgerMenu = document.getElementById("myLinks");
-    let desktop = document.getElementById("desktop");
-    let nav = document.getElementById("toptop");
-
-    if (burgerMenu.style.display === "flex") {
-        desktop.style.display = "block";
-        burgerMenu.style.display = "none";
-        nav.style.backgroundColor = "transparent";
+  }
+  
+  function mobileMenu() {
+    animateIcon();
+  
+    // +/- .mobile to .header-cont
+    let headerTop = document.getElementById("header-cont");
+    headerTop.classList.toggle("mobile");
+  
+    // +/- .hide to .header-main
+    let headerMain = document.getElementById("header-main");
+    headerMain.classList.toggle("hide");
+  
+    let navMenuLinks = document.getElementById("links");
+    let main = document.getElementsByClassName("noheader");
+  
+    if (navMenuLinks.style.display === "flex") {
+      // case press on  ===X=== icon
+      navMenuLinks.style.display = "none";
+      //on/off visibility of mobile version of nav-links
+      for (const x of main) {
+        x.style.display = "block";
+      }
+      //for-of ====> on/off visibility of main (no-header) blocks
     } else {
-        desktop.style.display = "none";
-        burgerMenu.style.display = "flex";
-        nav.style.backgroundColor = "var(--background-color)";
+      // case press on  ===E=== icon
+      navMenuLinks.style.display = "flex";
+      for (const x of main) {
+        x.style.display = "none";
+      }
     }
-}
+  }
+  
