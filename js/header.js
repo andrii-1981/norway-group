b2dce3733a1fe269f12d5a469dc62a0199b6271f
mmcxph <mmcxph@gmail.com>
2022-08-29 13:30:27 +0000

    edited burger.js

--- js/header.js
+++ js/header.js
@@ -1,37 +1,35 @@
-(function () {
+// (function () {
 
-  function animateIcon() {
-    // change X <=> E in burger icon
-    const x = document.getElementById("burger-icon");
-    x.classList.toggle("change");
-  }
+function animateIcon() {
+  // change symbol X <=> E in burger icon
+  const iconBurger = document.getElementById("burger-icon");
+  iconBurger.classList.toggle("change");
+}
+
+function mobileMenu() {  //open/close burger
+  const navMenuLinks = document.getElementById("links");
+  const body = document.getElementById("body");
+  const headerTop = document.getElementById("header-cont");
   
-  function mobileMenu() {
-    animateIcon();  // change X <=> E in burger icon
-    
-    const headerTop = document.getElementById("header-cont");// class .header-cont +/- class .mobile
+  if (window.screen.width <= 992) {
+    animateIcon();
+
     headerTop.classList.toggle("mobile");
-    
-    const navMenuLinks = document.getElementById("links");
-    const body = document.getElementById("body");
-    
+    //  .header-cont +/- class .mobile
+
     if (navMenuLinks.style.display === "flex") {
-    // case press on  ===X=== icon
-    navMenuLinks.style.display = "none";// on/off nav-links visibility in mobile version
-    body.classList.toggle("noscroll"); // body (id="body") +/- class .noscroll
-    
-  } else {
-    // case press on  ===E=== icon
-    navMenuLinks.style.display = "flex";
-    body.classList.toggle("noscroll");
+      // case press on  ===X=== icon:
+      navMenuLinks.style.display = "none";
+      body.classList.toggle("noscroll");
+      // on/off nav-links visibility in mobile version
+      // body (id="body") +/- class .noscroll
+
+    } else {
+      // case press on  ===E=== icon:
+      navMenuLinks.style.display = "flex";
+      body.classList.toggle("noscroll");
+    }
   }
 }
 
-function hrefAndCloseBurger (anchor){
-
-  
-  mobileMenu();  //close Burger (change icon E to icon X)
-  document.location.href = anchor;  // forward user to anchor
-}  
-
-})();
+// })()
