50389487e19ab553ad91958c61848867977f5310
halyna-h <haplykhalya@gmail.com>
2022-09-04 18:31:19 +0000

    added code

---
+++ js/join-formspree.js
@@ -0,0 +1,38 @@
+(function () {
+    const form = document.getElementById("join__form");
+  
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const status = document.getElementById("join__form__status");
+      const data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+          } else {
+            response.json().then((data) => {
+              if (Object.hasOwn(data, "errors")) {
+                status.innerHTML = data["errors"]
+                  .map((error) => error["message"])
+                  .join(", ");
+              } else {
+                status.innerHTML =
+                  "Oops! There was a problem submitting your form";
+              }
+            });
+          }
+        })
+        .catch((error) => {
+          status.innerHTML = "Oops! There was a problem submitting your form";
+        });
+    }
+    form.addEventListener("submit", handleSubmit);
+  })();
+  
